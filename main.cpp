7d19d08cf53c62ea0763d387f1c45b0ce177333f
Behan001 <niallmoyvane@hotmail.com>
2023-03-25 15:01:28 +0000

    include descriptive comments
    Signed-off-by: Behan001 <niallmoyvane@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include "mbed.h"
+#include "mbed.h" //include the header file "mbed.h"
 
-Serial pc(USBTX, USBRX);
-
-//term value
-int n=12;
+Serial pc(USBTX, USBRX); //creates a Serial object named "pc" for output to the computer's serial port
 
 
-int fib(int n){
+int n=6; //term value integer variable named "n" with the value 6
+
+
+int fib(int n){   // This function takes input n and returns the nth number (6) in the Fibonacci series.
     //1,2,3,4,5......
     //0,1,1,2,3
     if(n == 1)
@@ -18,7 +18,7 @@ int fib(int n){
         }
 }
 
-int main()
+int main() //Prints the fibonacci secquence on Putty app on the pc.
     {
     pc.printf("using recursive function (%d) \r\n", n);
     for(int i=1; i<=n; i++)
